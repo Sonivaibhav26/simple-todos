3b47ff3a8d40238a556c5ab5ded8ec0ea8c9ec18
vaibhav <vaibhav@apiria.com>
2018-01-18 17:05:45 +0530

    temporary-ui-state

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,12 +2,25 @@ import { Template } from 'meteor/templating';
 import { Tasks } from '../api/tasks.js';
 import './task.js';
 import './body.html';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 Template.body.helpers({
     tasks() {
-        // Show newest tasks at the top
-    return Tasks.find({}, { sort: { createdAt: -1 } });
+        const instance = Template.instance();
+        if (instance.state.get('hideCompleted')) {
+            // If hide completed is checked, filter tasks
+            return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+        }
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, { sort: { createdAt: -1 } });
     },
+    incompleteCount() {
+        return Tasks.find({ checked: { $ne: true } }).count();
+    },
+});
+
+Template.body.onCreated(function bodyOnCreated() {
+    this.state = new ReactiveDict();
 });
 
 Template.body.events({
@@ -27,5 +40,8 @@ Template.body.events({
 
         // Clear form
         target.text.value = '';
+    },
+    'change .hide-completed input'(event, instance) {
+        instance.state.set('hideCompleted', event.target.checked);
     },
 });
